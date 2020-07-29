0bf6d27a4295abeed23c1a4cc0c56e8732c8b7c6
Sebastian M. Ernst <ernst@pleiszenburg.de>
2020-07-29 13:35:12 +0200

    simple test dataset

--- makefile.py
+++ makefile.py
@@ -28,10 +28,14 @@ specific language governing rights and limitations under the License.
 # IMPORT
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
+import io
+import zipfile
 import os
 import sys
 
 import requests
+from tqdm import tqdm
+import xmltodict
 
 
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -55,11 +59,53 @@ MINOR_STEP = 2
 def make_testdata():
 
     for minor in range(MINOR_MIN, MINOR_MAX + MINOR_STEP, MINOR_STEP):
+
         r = requests.get(f"{REPO_DEFAULT_URL:s}?qgis=3.{minor:d}")
+        data = r.text
+
         with open(
-            os.path.join(TESTDATA_FLD, f"plugins_all_3-{minor:02d}.xml"), "w"
+            os.path.join(TESTDATA_FLD, f"plugins_3-{minor:02d}.xml"), "w"
         ) as f:
-            f.write(r.text)
+            f.write(data)
+
+        data = data.replace('& ', '&amp; ') # From plugin installer: Fix lonely ampersands in metadata
+        tree = xmltodict.parse(data)
+
+        for node in tqdm(tree['plugins']['pyqgis_plugin']):
+
+            assert node['version'] == node['@version']
+            assert node['file_name'].endswith('.zip')
+            assert node['version'] in node['file_name']
+
+            node_id = node['file_name'][:-1*(len('.zip') + len(node['version']) + len('.'))]
+
+            meta_fn = os.path.join(TESTDATA_FLD, f"metadata_{node_id:s}_{node['version']:s}.txt")
+            if os.path.exists(meta_fn):
+                continue
+
+            try:
+                r = requests.get(node['download_url'])
+                data = r.content
+                with io.BytesIO(data) as f:
+                    with zipfile.ZipFile(f, 'r') as fz:
+                        meta = fz.read(f'{node_id:s}/metadata.txt').decode('utf-8')
+                with open(meta_fn, "w") as f:
+                    f.write(meta)
+            except:
+                print(node)
+                if r.status_code == 404:
+                    print('HTTP 404')
+                    continue
+                raise
+
+            # print(f'{REPO_DEFAULT_URL:s}?package_name={node_id:s}&qgis=3.{minor:d}')
+            # r = requests.get(f'{REPO_DEFAULT_URL:s}?package_name={node_id:s}&qgis=3.{minor:d}')
+            # data = r.text
+            #
+            # with open(
+            #     os.path.join(TESTDATA_FLD, f"plugins_3-{minor:02d}_{node_id:s}.xml"), "w"
+            # ) as f:
+            #     f.write(data)
 
 
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
